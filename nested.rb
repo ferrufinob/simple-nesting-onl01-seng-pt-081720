17bc20d51345610c1941198b2c1efeb869b0cf9c
Brenda Ferrufino <ferrufinob@gmail.com>
2020-08-25 01:22:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,8 +85,8 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 new_hash = { :yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]}}
-    programmer_hash.merge(new_hash)
-    programmer_hash
+    new_hash.merge(programmer_hash)
+   
 end
 
 def changing_alan
